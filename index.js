442d81c701721d7b28461174f08f2afe6ee7c4de
Pipaplutarco <dereksamuelgr@gmail.com>
2021-12-27 18:22:19 -0500

    sensor

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 const $scene = document.querySelector("a-scene");
 const $content = document.querySelector(".content");
-const earthRadius = 6.371;// in km
 
 window.objective = {
-  lat: 4.579164687575145,
-  lon: -74.1212180212868
+  lat: 4.578540040534844,
+  lon: -74.12260975377717
 };
 
 function initialize() {
   getLocation();
-  // 1 = 111111 m
 }
 
 function getLocation() {
-  navigator.geolocation.watchPosition((position) => {
-    window.myLocation = {
-      lat: position.coords.latitude,
-      lon: position.coords.longitude,
-    };
+  const options = { frequency: 60, referenceFrame: 'device' };
+  const sensor = new AbsoluteOrientationSensor(options);
 
-    const totalDistance = "";
-
-    // const zObject3D = myLocation.lat > objective.lat ? myLocation.lat - objective.lat : objective.lat - myLocation.lat;
-    // const xObject3D = myLocation.lon > objective.lon ? myLocation.lon - objective.lon : objective.lon - myLocation.lon;
-
-    // Create a new 3d object
-    // const $aBox = document.createElement("a-box");
-
-    // $aBox.setAttribute("position", `${objective.lon} 0 ${objective.lat}`);
-    // $aBox.setAttribute("color", "#4CC3D9");
-
-    // $scene.appendChild($aBox);
-    $content.innerHTML = `
-      <p>Lat: ${Math.hypot(myLocation.lat, objective.lat)}</p>
-      <p>Lon: ${Math.hypot(myLocation.lon, objective.lon)}m</p>
-    `;
-  }, console.error);
+  sensor.addEventListener('reading', () => {
+    // model is a Three.js object instantiated elsewhere.
+    model.quaternion.fromArray(sensor.quaternion).inverse();
+  });
+  sensor.addEventListener('error', error => {
+    if (event.error.name == 'NotReadableError') {
+      console.log("Sensor is not available.");
+    }
+  });
+  sensor.start();
 }
 
 window.onload = initialize();
