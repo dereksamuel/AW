cf02d5af9db3a3e0af4693f18fa55db8f54ef8ba
Pipaplutarco <dereksamuelgr@gmail.com>
2021-12-27 15:08:57 -0500

    test measure

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const $scene = document.querySelector("a-scene");
-const earthMeridian = 111111.11111111111; // in meters
+const $content = document.querySelector(".content");
 const earthRadius = 6.371;// in km
 
-// Math.PI * earthRadius * (4.580844580492296 / 180)
 window.objective = {
-  lat: 4.579568831770866,
-  lon: -74.12089832800635
+  lat: 4.579830355454221,
+  lon: -74.12097647667673
 };
 
 function initialize() {
@@ -22,17 +21,38 @@ function getLocation() {
       lon: position.coords.longitude,
     };
 
+    const totalDistance = measure(
+      window.myLocation.lat,
+      window.myLocation.lon,
+      window.objective.lat,
+      window.objective.lon
+    );
+
     // const zObject3D = myLocation.lat > objective.lat ? myLocation.lat - objective.lat : objective.lat - myLocation.lat;
     // const xObject3D = myLocation.lon > objective.lon ? myLocation.lon - objective.lon : objective.lon - myLocation.lon;
 
     // Create a new 3d object
-    const $aBox = document.createElement("a-box");
+    // const $aBox = document.createElement("a-box");
 
-    $aBox.setAttribute("position", `${objective.lon} 0 ${objective.lat}`);
-    $aBox.setAttribute("color", "#4CC3D9");
+    // $aBox.setAttribute("position", `${objective.lon} 0 ${objective.lat}`);
+    // $aBox.setAttribute("color", "#4CC3D9");
 
-    $scene.appendChild($aBox);
+    // $scene.appendChild($aBox);
+    $content.innerHTML = `
+      <p>Lat: ${totalDistance}</p>
+      <p>Lon: ${totalDistance}</p>
+    `;
   }, console.error);
+}
+
+function measure(lat1, lon1, lat2, lon2){ //
+  var R = 6378.137;
+  var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+  var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d * 1000;// meters
 }
 
 window.onload = initialize();
