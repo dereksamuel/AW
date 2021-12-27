382b8b1bc2347d825d88af2a7e207e40d263de2e
Pipaplutarco <dereksamuelgr@gmail.com>
2021-12-27 18:30:23 -0500

    sensor

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const $scene = document.querySelector("a-scene");
-const $content = document.querySelector(".content");
-
-window.objective = {
-  lat: 4.578540040534844,
-  lon: -74.12260975377717
-};
-
-function initialize() {
-  getLocation();
-}
-
-function getLocation() {
-  const options = { frequency: 60, referenceFrame: 'device' };
-  const sensor = new AbsoluteOrientationSensor(options);
-
-  sensor.addEventListener('reading', () => {
-    // model is a Three.js object instantiated elsewhere.
-    model.quaternion.fromArray(sensor.quaternion).inverse();
-  });
-  sensor.addEventListener('error', error => {
-    if (event.error.name == 'NotReadableError') {
-      console.log("Sensor is not available.");
+const sensor = new AbsoluteOrientationSensor();
+Promise.all([navigator.permissions.query({ name: "accelerometer" }),
+        navigator.permissions.query({ name: "magnetometer" }),
+        navigator.permissions.query({ name: "gyroscope" })])
+  .then(results => {
+    if (results.every(result => result.state === "granted")) {
+      sensor.start();
+      alert(sensor);
+    } else {
+      console.log("No permissions to use AbsoluteOrientationSensor.");
     }
-  });
-  sensor.start();
-}
-
-window.onload = initialize();
+});
